6f2e093e9a2c486833204bf653cc5d6ce83c78f6
Dustan Levenstein <dlevenstein@gmail.com>
2020-05-25 18:49:46 -0500

    Processing csv's.

--- data_scraper.py
+++ data_scraper.py
@@ -8,7 +8,12 @@ Created on Thu Apr 16 17:08:26 2020
 
 import os
 import pandas as pd
+dataframes = {}
 for directory, dirnames, filenames in os.walk("."):
     for filename in filenames:
         if filename[-3:] == 'csv':
-            print("Found a csv!")
+            fullpath = os.path.join(directory, filename)
+            dataframes[fullpath] = pd.read_csv(fullpath)
+
+keys = list(dataframes.keys())
+keys.sort()
