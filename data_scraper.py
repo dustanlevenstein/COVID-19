79434e17265150067d437da130bbd823885bf072
Dustan Levenstein <dlevenstein@gmail.com>
2020-05-25 19:33:54 -0500

    Observe the evolution of the column names.

--- data_scraper.py
+++ data_scraper.py
@@ -23,3 +23,45 @@ keys.sort()
 standardized_keys = [pathname for pathname in keys 
                      if os.path.split(os.path.split(pathname)[0])[-1]
                          == 'csse_covid_19_daily_reports']
+
+def extract_ymd(filepath):
+    return tuple(map(int, os.path.split(filepath)[-1].split('.')[0].split('-'))
+                 )
+
+dated_data_frames = {extract_ymd(filepath) : dataframes[filepath]
+                     for filepath in standardized_keys}
+columns = {}
+previous_columns = None
+for key in sorted(dated_data_frames.keys()):
+    if tuple(dated_data_frames[key].columns) != previous_columns:
+        previous_columns = tuple(dated_data_frames[key].columns)
+        columns[key] = previous_columns
+        
+# columns
+# Out[26]: 
+# {(1, 22, 2020): ('Province/State',
+#   'Country/Region',
+#   'Last Update',
+#   'Confirmed',
+#   'Deaths',
+#   'Recovered'),
+#  (3, 1, 2020): ('Province/State',
+#   'Country/Region',
+#   'Last Update',
+#   'Confirmed',
+#   'Deaths',
+#   'Recovered',
+#   'Latitude',
+#   'Longitude'),
+#  (3, 22, 2020): ('FIPS',
+#   'Admin2',
+#   'Province_State',
+#   'Country_Region',
+#   'Last_Update',
+#   'Lat',
+#   'Long_',
+#   'Confirmed',
+#   'Deaths',
+#   'Recovered',
+#   'Active',
+#   'Combined_Key')}
